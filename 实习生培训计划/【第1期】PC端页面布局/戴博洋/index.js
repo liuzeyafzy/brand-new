1f373af1c9a7772d19cb29f1ea1dbb1a187521fe
“White-lamb” <1756053204@qq.com>
2019-03-04 09:29:15 +0800

    周末

--- 实习生培训计划/【第1期】PC端页面布局/戴博洋/index.js
+++ 实习生培训计划/【第1期】PC端页面布局/戴博洋/index.js
@@ -1,18 +1,50 @@
 window.onload = function(){
-    var set = document.getElementById("set")
-    var sets = document.getElementById("sets")
-    var more = document.getElementsByClassName("more")[0]
-    var moremore = document.getElementById("more-more")
-    set.onmouseover = function(){
-        sets.style.display = "block"
+    var weathermore = document.getElementsByClassName("weathermore")[0]
+    var wMore = document.getElementsByClassName("w-more")[0]
+
+    weathermore.onmouseover = function(){
+        wMore.style.display = "block"
     }
-    set.onmouseout = function(){
-        sets.style.display = "none"
+    weathermore.onmouseout = function(){
+        wMore.style.display = "none"
     }
-    more.onmouseover = function(){
-        moremore.style.display = "block"
+
+
+    var usermore = document.getElementsByClassName("usermore")[0]
+    var rUserUl = document.getElementsByClassName("r-user-ul")[0]
+
+    usermore.onmouseover = function(){
+        rUserUl.style.display = "block"
     }
-    more.onmouseout = function(){
-        moremore.style.display = "none"
+    usermore.onmouseout = function(){
+        rUserUl.style.display = "none"
     }
+
+    var sets = document.getElementsByClassName("sets")[0]
+    var rSetUl = document.getElementsByClassName("r-set-ul")[0]
+
+    sets.onmouseover = function(){
+        rSetUl.style.display = "block"
+    }
+    sets.onmouseout = function(){
+        rSetUl.style.display = "none"
+    }
+
+    var rightMores = document.getElementsByClassName("right-mores")[0]
+    var rMoreUl = document.getElementsByClassName("r-more-ul")[0]
+    var rightMore = document.getElementsByClassName("right-more")[0]
+    rightMores.onmouseover = function(){
+        rMoreUl.style.display = "block"
+        rightMore.style.backgroundColor = "#f0f0f0"
+        rightMore.style.color = "#000"
+        rightMore.style.height = "41px"
+    }
+    rightMores.onmouseout = function(){
+        rMoreUl.style.display = "none"
+        rightMore.style.backgroundColor = "#398bfb"
+        rightMore.style.color = "#fff"
+        rightMore.style.height = "32px"
+    }
+
+
 }
